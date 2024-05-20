1e1bf3627d388071b46e043d23a4e16e1df85212
mihanik575 <m9854737384@gmail.com>
2024-05-20 20:23:58 +0530

    commit

---
+++ Leaderboard climbers.py
@@ -0,0 +1,17 @@
+leaderboard = ['John', 'Brian', 'Jim', 'Dave', 'Fred']
+changes = ['Dave +1', 'Fred +4', 'Brian -1']
+
+# def leaderboard_sort(leaderboard, changes):
+for i in range(len(leaderboard)):
+    pass
+list_of_changes = []
+for i in range(len(changes)):
+    list_of_changes.append(changes[i].split())
+
+print(list_of_changes)
+
+print(int(list_of_changes[2][1]))
+
+MyList.insert(index_to_insert, MyList.pop(index_to_remove))
+
+# leaderboard_sort(leaderboard, changes)
