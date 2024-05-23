8f6d9d4de661d019a10174a2bba8272edb408092
mihanik575 <m9854737384@gmail.com>
2024-05-23 20:40:40 +0530

    commit

--- Leaderboard climbers.py
+++ Leaderboard climbers.py
@@ -1,26 +1,18 @@
 leaderboard = ['John', 'Brian', 'Jim', 'Dave', 'Fred']
 changes = ['Dave +1', 'Fred +4', 'Brian -1']
 
-# def leaderboard_sort(leaderboard, changes):
 list_of_changes = []
 for i in range(len(changes)):
     list_of_changes.append(changes[i].split())
-list_do = []
-list_posle = []
 
-# for x in range(len(leaderboard)):
-#     for y in range(len(list_of_changes)):
-#         if leaderboard[x] == list_of_changes[y][0]:
-#             leaderboard.insert(leaderboard[x], leaderboard.pop(list_of_changes[y]))
 print(leaderboard)
-leaderboard.insert(leaderboard.index(leaderboard[2]), leaderboard.pop(3))
-print(leaderboard)
-# print(leaderboard.pop(3))
+print(list_of_changes[2][1])
+for y in range(len(list_of_changes)):
+    for x in range(len(leaderboard)):
+        if leaderboard[x] == list_of_changes[y][0]:
+            leaderboard.insert(int(leaderboard.index(leaderboard[x])) - int(list_of_changes[y][1]), leaderboard.pop(x))
+            print(int(leaderboard.index(leaderboard[x])) - int(list_of_changes[y][1]))
+            print(leaderboard)
 
-# print(list_of_changes)
-# print(leaderboard)
-# print(int(list_of_changes[2][1]))
 
-# MyList.insert(index_to_insert, MyList.pop(index_to_remove))
 
-# leaderboard_sort(leaderboard, changes)
